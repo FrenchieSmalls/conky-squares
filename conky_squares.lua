c964e33b0705d85bb849f41987256d82cf50ca07
FrenchieSmalls <frenchiesmalls@gmail.com>
2017-05-29 15:35:20 +0800

    Added randomized color blocks and battery percentage block, changed date and font.

--- conky_squares.lua
+++ conky_squares.lua
@@ -4,14 +4,25 @@ COLOR_FONT_R = 0.9
 COLOR_FONT_G = 0.9
 COLOR_FONT_B = 0.9
 
-COLOR_PRIMARY_R = 0.70
-COLOR_PRIMARY_G = 0.75
-COLOR_PRIMARY_B = 0.96
+COLOR_PRIMARY_R = 0.698
+COLOR_PRIMARY_G = 0.753
+COLOR_PRIMARY_B = 0.961
 
-COLOR_SECONDARY_R = 0.26
-COLOR_SECONDARY_G = 0.40
-COLOR_SECONDARY_B = 0.85
+COLOR_SECONDARY_R = 0.263
+COLOR_SECONDARY_G = 0.404
+COLOR_SECONDARY_B = 0.851
 
+COLOR_BACKGROUND_R = 0.165
+COLOR_BACKGROUND_G = 0.192
+COLOR_BACKGROUND_B = 0.251
+
+COLOR_BOX1_R = 0.216
+COLOR_BOX1_G = 0.404
+COLOR_BOX1_B = 0.651
+
+COLOR_BOX2_R = 0.388
+COLOR_BOX2_G = 0.420
+COLOR_BOX2_B = 0.949	
 
 function init_cairo()
   if conky_window == nil then
@@ -27,7 +38,7 @@ function init_cairo()
 
   cr = cairo_create(cs)
 
-  font = "Anadale Mono"
+  font = "Overpass"
 
   cairo_select_font_face(cr, font, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL)
   cairo_set_source_rgba(cr, COLOR_FONT_R, COLOR_FONT_G, COLOR_FONT_B, 1)
@@ -49,11 +60,16 @@ function conky_main()
   
   
   -- DATE
-  cairo_set_font_size(cr, 24)
+  cairo_set_font_size(cr, 40)
   cairo_set_source_rgba(cr, COLOR_FONT_R, COLOR_FONT_G, COLOR_FONT_B, 0.9)
-  cairo_move_to(cr, 63, 182)
-  local time_str = string.format('%-12s', conky_parse("${time %A,}"))..conky_parse("${time %d.%m.%Y}")
+  cairo_move_to(cr, 64, 185)
+  local time_str = string.format('%-12s',conky_parse("${time %d/%m/%Y}"))
   cairo_show_text(cr, time_str)
+  cairo_stroke(cr)
+
+  local day_str = string.format('%-12s', conky_parse("${time %A}"))
+  cairo_move_to(cr, 64, 225)
+  cairo_show_text(cr, day_str)
   cairo_stroke(cr)
 
 
@@ -134,8 +150,6 @@ function conky_main()
   cairo_rel_line_to(cr, -width, 0)
   cairo_fill(cr)
 
-
-
   
   -- MEMORY
   
@@ -145,7 +159,7 @@ function conky_main()
   local rows = 5
   local perc = 0.0
   local perc_incr = 100.0 / 40.0
-  local cx,cy = 520,490
+  local cx,cy = 520,495
   local grid_width = 36
   for i = 1,40 do
     if (memperc > perc) then
@@ -168,7 +182,86 @@ function conky_main()
     perc = perc + perc_incr
   end
 
+
+
+  -- RANDOMIZED BACKGROUND SQUARES
+  rand_col = math.random()
+  rand_box(rand_col,29,687,320)
+  
+  rand_col = math.random()
+  rand_box(rand_col,29,687,433)
+ 
+  rand_col = math.random()
+  rand_box(rand_col,29,828,433)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,800,320)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,800,461)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,857,575)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,970,547)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,1027,644)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,998,519)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,1027,490)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,1055,320)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,1167,320)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,1252,461)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,913,320)
+
+  rand_col = math.random()
+  rand_box(rand_col,29,998,264)
+
+
+
+  -- BATTERY PERCENTAGE
+
+  batt = tonumber(conky_parse("${battery_percent BAT0}"))
+
+  cairo_show_text(cr, batt)
+  cairo_stroke(cr)
+
+  local cx,cy = 1160,441
+  local width,height = 100,100
+  local volume_height = 100
+  local filled_height = volume_height * batt / 100
+  local line_width = 5
+
+  cairo_set_source_rgba(cr, COLOR_BOX2_R, COLOR_BOX2_G, COLOR_BOX2_B, 1)
+  cairo_move_to(cr, cx, cy)
+  cairo_rel_line_to(cr, width, 0)
+  cairo_rel_line_to(cr, 0, -volume_height)
+  cairo_rel_line_to(cr, -width, 0)
+  cairo_fill(cr)
+
+  cairo_set_source_rgba(cr, COLOR_BOX1_R, COLOR_BOX1_G, COLOR_BOX1_B, 1)
+  cairo_move_to(cr, cx, cy)
+  cairo_rel_line_to(cr, width, 0)
+  cairo_rel_line_to(cr, 0, -filled_height)
+  cairo_rel_line_to(cr, -width, 0)
+  cairo_fill(cr)
 end
+
+
+
 
 -- FILE SYSTEM
 
@@ -181,10 +274,10 @@ function conky_fs_main()
   local gap = 79
   local dn = dbox_used("${exec du -hs /home/chris/Dropbox/ | head -n1 | awk '{print $1}'}")
 
-  draw_volume("         /", tonumber(conky_parse("${fs_used_perc /}")) , offset)
-  draw_volume("  Backups", tonumber(conky_parse("${fs_used_perc /media/chris/Backups/}")) , offset + gap)
+  draw_volume("             /", tonumber(conky_parse("${fs_used_perc /}")) , offset)
+  draw_volume("    Backups", tonumber(conky_parse("${fs_used_perc /media/chris/Backups/}")) , offset + gap)
   
-  draw_volume("  Dropbox", dn , offset + 2*gap)
+  draw_volume("    Dropbox", dn , offset + 2*gap)
   
 
   cairo_destroy(cr)
@@ -192,14 +285,34 @@ function conky_fs_main()
   cr = nil
 end
 
+
+
 function dbox_used(arg)
   local str = conky_parse(arg)
   local n = tonumber(str:sub(1,-2)) / 2.5
   return n*100
 end
 
+
+function rand_box(rand_col,box_size,cx,cy)
+  if (rand_col < 0.2) then
+    cairo_set_source_rgba(cr, COLOR_PRIMARY_R, COLOR_PRIMARY_G, COLOR_PRIMARY_B, 1)
+  elseif (rand_col >= 0.2 and rand_col < 0.4) then
+    cairo_set_source_rgba(cr, COLOR_SECONDARY_R, COLOR_SECONDARY_G, COLOR_SECONDARY_B, 1)
+  elseif (rand_col >= 0.4 and rand_col < 0.6) then
+    cairo_set_source_rgba(cr, COLOR_BOX1_R, COLOR_BOX1_G, COLOR_BOX1_B, 1)
+  elseif (rand_col >= 0.6 and rand_col < 0.8) then
+    cairo_set_source_rgba(cr, COLOR_BOX2_R, COLOR_BOX2_G, COLOR_BOX2_B, 1)
+  else
+    cairo_set_source_rgba(cr, COLOR_BACKGROUND_R, COLOR_BACKGROUND_G, COLOR_BACKGROUND_B, 1) 
+  end
+  cairo_rectangle(cr, cx-box_size/4, cy-box_size/4, box_size/2, box_size/2)
+  cairo_fill(cr)
+end
+
+
 function draw_volume(name, used, cx)
-  local cy = 643
+  local cy = 652
   local width,height = 55,15
   local volume_height = 140
   local filled_height = volume_height * used / 100
